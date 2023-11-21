7eec7e4129ae09cdfdb9bedd3d753606d32f9380
Tijmen Verhoef <tijmenmenno@gmail.com>
2023-11-21 15:01:37 +0100

    moved rt pass to separate struct

--- src/renderer.rs
+++ src/renderer.rs
@@ -2,7 +2,7 @@ mod gl_layer;
 
 use std::path::Path;
 use nalgebra_glm::Vec3;
-use image::{RgbImage, ImageBuffer, Rgb, EncodableLayout};
+use image::{RgbImage, Rgb, EncodableLayout};
 
 use gl_layer::{
     vertex_array::VertexArray,
@@ -23,6 +23,105 @@ pub struct RendererConfig {
     pub resolution: Resolution,
     pub sample_count: u32,
     pub max_bounces: u32,
+}
+
+struct RayTracingPass {
+    resolution: Resolution,
+    sample_count: u32,
+    max_bounces: u32,
+    render_target: RgbImage,
+}
+
+impl RayTracingPass {
+    pub fn new(resolution: Resolution, sample_count: u32, max_bounces: u32) -> Self {
+        RayTracingPass {
+            resolution,
+            sample_count,
+            max_bounces,
+            render_target: RgbImage::new(resolution.width(), resolution.height()),
+        }
+    }
+
+    pub fn execute(&mut self, camera: &Camera, scene: &Scene) {
+        let z_interval = camera.scene_depth_interval();
+
+        for y in 0..self.resolution.height() {
+            for x in 0..self.resolution.width() {
+                let mut sample_sum_color = Vec3::zeros();
+
+                for sample in 0..self.sample_count {
+                    let ray = self.get_ray(camera, x, y, sample);
+                    let color = self.bounce_ray(&ray, scene, z_interval, self.max_bounces);
+
+                    sample_sum_color += color;
+                }
+    
+                let color = sample_sum_color / self.sample_count as f32;
+                let color = Self::rgb_to_gamma(color);
+                let color = Self::vec3_to_color(&color);
+                self.render_target.put_pixel(x, y, color);
+            }
+        }
+    }
+
+    fn get_ray(&self, camera: &Camera, x: u32, y: u32, sample: u32) -> Ray {
+        let pixel_center = camera.get_pixel_center(x, y);
+        let pixel_sample = camera.sample_pixel(pixel_center, sample);
+
+        let ray_origin = camera.get_ray_origin();
+        let ray_direction = pixel_sample - ray_origin;
+        let ray_direction = ray_direction.normalize();
+
+        Ray::new(ray_origin, ray_direction)
+    }
+
+    fn bounce_ray(&self, ray: &Ray, scene: &Scene, z_interval: &Interval, depth: u32) -> Vec3 {
+        if depth == 0 {
+            return Vec3::zeros();
+        }
+
+        let closest_hit = scene.hit(ray, z_interval);
+        match closest_hit {
+            Some(hit) => {
+                let scatter = hit.material.scatter(ray, &hit);
+
+                match scatter {
+                    Some(scatter) => {
+                        let object_color = scatter.attenuation.component_mul(&self.bounce_ray(&scatter.ray, scene, z_interval, depth - 1));
+
+                        let light_color = match hit.material.material_transparency() {
+                            MaterialTransparency::Opaque => scene.shadow_ray(&hit, z_interval),
+                            MaterialTransparency::Transparent => Vec3::new(1.0, 1.0, 1.0),
+                        };
+
+                        return object_color.component_mul(&light_color)
+                    },
+                    None => return Vec3::zeros(),
+                }
+            }
+            None => {
+                return scene.get_sky_color(&ray);
+            }
+        }
+    }
+
+    fn rgb_to_gamma(color: Vec3) -> Vec3 {
+        Vec3::new(
+            f32::sqrt(color.x),
+            f32::sqrt(color.y),
+            f32::sqrt(color.z),
+        )
+    }
+
+    fn vec3_to_color(color: &Vec3) -> Rgb<u8> {
+        let intensity = Interval::new(0.0, 0.999);
+
+        Rgb([
+            (intensity.clamp(color.x) * 256.0) as u8,
+            (intensity.clamp(color.y) * 256.0) as u8,
+            (intensity.clamp(color.z) * 256.0) as u8,
+        ])
+    }
 }
 
 struct RasterPass {
@@ -94,8 +193,7 @@ impl RasterPass {
 }
 
 pub struct Renderer {
-    config: RendererConfig,
-    render_target: RgbImage,
+    ray_tracing_pass: RayTracingPass,
     raster_pass: RasterPass,
 }
 
@@ -103,100 +201,21 @@ impl Renderer {
     pub fn new(window: &mut glfw::Window, config: &RendererConfig) -> Self {
         gl_layer::init(window);
 
-        let render_target: RgbImage = ImageBuffer::new(config.resolution.width(), config.resolution.height());
+        let ray_tracing_pass = RayTracingPass::new(config.resolution, config.sample_count, config.max_bounces);
         let raster_pass = RasterPass::new(&config.resolution);
 
         Renderer {
-            config: *config,
-            render_target,
+            ray_tracing_pass,
             raster_pass,
         }
     }
 
     pub fn render(&mut self, camera: &Camera, scene: &Scene) {
-        let z_interval = camera.scene_depth_interval();
-
-        for y in 0..self.config.resolution.height() {
-            for x in 0..self.config.resolution.width() {
-                let mut sample_sum_color = Vec3::zeros();
-
-                for sample in 0..self.config.sample_count {
-                    let ray = self.get_ray(camera, x, y, sample);
-                    let color = self.bounce_ray(&ray, scene, z_interval, self.config.max_bounces);
-
-                    sample_sum_color += color;
-                }
-    
-                let color = sample_sum_color / self.config.sample_count as f32;
-                let color = Self::rgb_to_gamma(color);
-                let color = Self::vec3_to_color(&color);
-                self.render_target.put_pixel(x, y, color);
-            }
-        }
-
-        self.raster_pass.execute(&self.render_target);
+        self.ray_tracing_pass.execute(camera, scene);
+        self.raster_pass.execute(&self.ray_tracing_pass.render_target);
     }
 
     pub fn save_render(&self, path: &Path) {
-        self.render_target.save(path).expect("Failed to save output image");
-    }
-
-    fn get_ray(&self, camera: &Camera, x: u32, y: u32, sample: u32) -> Ray {
-        let pixel_center = camera.get_pixel_center(x, y);
-        let pixel_sample = camera.sample_pixel(pixel_center, sample);
-
-        let ray_origin = camera.get_ray_origin();
-        let ray_direction = pixel_sample - ray_origin;
-        let ray_direction = ray_direction.normalize();
-
-        Ray::new(ray_origin, ray_direction)
-    }
-
-    fn bounce_ray(&self, ray: &Ray, scene: &Scene, z_interval: &Interval, depth: u32) -> Vec3 {
-        if depth == 0 {
-            return Vec3::zeros();
-        }
-
-        let closest_hit = scene.hit(ray, z_interval);
-        match closest_hit {
-            Some(hit) => {
-                let scatter = hit.material.scatter(ray, &hit);
-
-                match scatter {
-                    Some(scatter) => {
-                        let object_color = scatter.attenuation.component_mul(&self.bounce_ray(&scatter.ray, scene, z_interval, depth - 1));
-
-                        let light_color = match hit.material.material_transparency() {
-                            MaterialTransparency::Opaque => scene.shadow_ray(&hit, z_interval),
-                            MaterialTransparency::Transparent => Vec3::new(1.0, 1.0, 1.0),
-                        };
-
-                        return object_color.component_mul(&light_color)
-                    },
-                    None => return Vec3::zeros(),
-                }
-            }
-            None => {
-                return scene.get_sky_color(&ray);
-            }
-        }
-    }
-
-    fn rgb_to_gamma(color: Vec3) -> Vec3 {
-        Vec3::new(
-            f32::sqrt(color.x),
-            f32::sqrt(color.y),
-            f32::sqrt(color.z),
-        )
-    }
-
-    fn vec3_to_color(color: &Vec3) -> Rgb<u8> {
-        let intensity = Interval::new(0.0, 0.999);
-
-        Rgb([
-            (intensity.clamp(color.x) * 256.0) as u8,
-            (intensity.clamp(color.y) * 256.0) as u8,
-            (intensity.clamp(color.z) * 256.0) as u8,
-        ])
+        self.ray_tracing_pass.render_target.save(path).expect("Failed to save output image");
     }
 }
